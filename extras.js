8f222bdef0e576e5c5b8be9b3195ac819a1c7ab9
Subin <sub_1n@yahoo.com>
2023-10-08 14:53:45 +0300

    commit 1extras

--- extras.js
+++ extras.js
@@ -1,1 +1,1 @@
-console.log("2" + 2); // result => 22; string concadination
+console.log("2" + 2); // result => 22; javascript can add a number and string by pushing the number behind the string as like concadination.
