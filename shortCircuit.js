fd729809474d66df79ded45a46a5c8b46bfde5d6
Subin <sub_1n@yahoo.com>
2023-10-08 13:37:38 +0300

    shortcircuit

---
+++ shortCircuit.js
@@ -0,0 +1,18 @@
+// sometime short circuit can replace if-else CSSLayerStatementRule.
+
+// let x = 5;
+// let y = 2;
+// let z = 10;
+
+// and => if there is any expression that is false, the whole expression is false.
+// let result = x > z && x > y;
+
+// or => if there is any expression that is true, the whole expression is true.
+
+//let result = x > y || x > z;
+
+// x > y && console.log("Hello, world!");
+
+// or case
+// let y = null || 10;
+// console.log(y);
