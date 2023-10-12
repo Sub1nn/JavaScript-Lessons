975d98193e1b5f816e15a6167a52fe3ee4ec3384
Subin <sub_1n@yahoo.com>
2023-10-13 01:32:06 +0300

    spread_rest_practice

--- spreadRest.operators.js
+++ spreadRest.operators.js
@@ -2,7 +2,7 @@
 // copying Arrays
 const originalArray = [1, 2, 3, 4, 5];
 let copyArray = [...originalArray];
-console.log(copyArray);
+// console.log(copyArray);
 //Adding two arrays
 const arr1 = ["a", "b", "c", "d"];
 const arr2 = ["e", "f", "g"];
@@ -13,3 +13,10 @@ console.log(arr3);
 const greeting = "Hello";
 const greet = [...greeting];
 console.log(greet);
+
+//Rest Operators
+//Used in functions parameters to collect multiple arguments.
+let sum = (...numbers) => {
+  return numbers.reduce((a, b) => a + b, 0);
+};
+console.log(sum(1, 2, 3, 4));
