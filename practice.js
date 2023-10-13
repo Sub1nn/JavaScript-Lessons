cf7367ebcc6e2d7341d8ba09bb340b3331472212
Subin <sub_1n@yahoo.com>
2023-10-13 18:01:11 +0300

    Object Copy Practice

--- practice.js
+++ practice.js
@@ -2,14 +2,32 @@
 //check if user is adult or not
 //add a field isAdult and set it to true/false
 
+// 1st way without defining object
+// const checkIfAdult = (person) => {
+//   console.log(person);
+//   let isAdult = false;
+//   if (person.age > 18) {
+//     isAdult = true;
+//   }
+//   return { ...person, isAdult: isAdult };
+// };
+
+// let result = checkIfAdult({ name: "Subin", age: "30", address: "Finland" });
+// console.log(result);
+
+// another way by defining person object first
+const person = {
+  name: "Subin",
+  age: "30",
+  address: "Finland",
+};
+
 const checkIfAdult = (person) => {
-  console.log(person);
   let isAdult = false;
   if (person.age > 18) {
     isAdult = true;
   }
-  return { ...person, isAdult: isAdult };
+  return { ...person, adult: isAdult };
 };
-
-let result = checkIfAdult({ name: "Subin", age: "30", address: "Finland" });
+const result = checkIfAdult({ ...person });
 console.log(result);
