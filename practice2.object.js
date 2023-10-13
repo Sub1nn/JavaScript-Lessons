14b408c53bba04c03a8896707350b06906095946
Subin <sub_1n@yahoo.com>
2023-10-13 19:00:47 +0300

    destructuring_practice

--- practice2.object.js
+++ practice2.object.js
@@ -1,7 +1,7 @@
 // if the key and value names are same we can just simply omit the value name
 
-const firstName = "Subin";
-const lastName = "Khatiwada";
+// const firstName = "Subin";
+// const lastName = "Khatiwada";
 
 // const person = {
 //   firstName: firstName,
@@ -10,8 +10,40 @@ const lastName = "Khatiwada";
 // console.log(person);
 
 //since the first name and last name is same, we can do as follows:
-const person = {
-  firstName,
-  lastName,
+// const person = {
+//   firstName, // js understands there is some value with the same name as key
+//   lastName,
+// };
+// console.log(person);
+
+// ? exe. object of personDetails => name, country
+// if country is Nepal, return object with isNepali true/false
+
+// const checkIfNepali = ({ name, country }) => {
+//   let isNepali = false;
+//   if (country === "Nepal") {
+//     isNepali = true;
+//   }
+//   return { name, country, isNepali: isNepali };
+// };
+
+// let result = checkIfNepali({ name: "Subin", country: "Nepal" });
+// console.log(result);
+
+// ! alter. way by first defining the object and also using destructing
+
+const personDetails = {
+  name: "Subin",
+  country: "Nepal",
 };
-console.log(person);
+
+const checkIfNepali = ({ country }) => {
+  let isNepali = false;
+  if (country === "Nepal") {
+    isNepali = true;
+  }
+  return { ...personDetails, isNepali: isNepali };
+};
+
+let result = checkIfNepali({ ...personDetails });
+console.log(result);
