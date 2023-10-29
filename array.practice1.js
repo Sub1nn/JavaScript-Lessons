732c2f397fb3237aa89740dc9844b6eddb195e01
Subin <sub_1n@yahoo.com>
2023-10-29 13:31:30 +0200

    practice_session 29.10

--- array.practice1.js
+++ array.practice1.js
@@ -1,6 +1,6 @@
 // ? The maximum sum Subarray
 
-let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 // const maxSequence = (arr) => {
 //   if (arr.length === 0) {
@@ -23,20 +23,34 @@ let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 // ! second method below
 
-const maxSequence = (arr) => {
-  let currentSum = 0;
-  let maxSum = 0;
-  for (let item of arr) {
-    currentSum += item;
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-    }
-    if (currentSum < 0) {
-      currentSum = 0;
-    }
-  }
-  return maxSum;
-};
+// const maxSequence = (arr) => {
+//   let currentSum = 0;
+//   let maxSum = 0;
+//   for (let item of arr) {
+//     currentSum += item;
+//     if (currentSum > maxSum) {
+//       maxSum = currentSum;
+//     }
+//     if (currentSum < 0) {
+//       currentSum = 0;
+//     }
+//   }
+//   return maxSum;
+// };
 
-let result = maxSequence(arr);
-console.log(result);
+// let result = maxSequence(arr);
+// console.log(result);
+
+// ? In the given array of marks if marks is greater or equal to 32 return marks but if it is less then 32 by just 5 marks, pass him/her by changing the marks to 32, else return marks.
+
+let marks = [25, 35, 31, 29, 55, 65];
+let passMarks = 32;
+let newMarks = marks.map((item) => {
+  if (item >= passMarks) {
+    return item;
+  } else if (item < passMarks && item >= passMarks - 5) {
+    //item += 5;
+    return passMarks;
+  } else return item;
+});
+console.log(newMarks);
