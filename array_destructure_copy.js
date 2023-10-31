c4f018eaeb1ea148b9328822704d9d111cbdeb63
Subin <sub_1n@yahoo.com>
2023-10-31 21:54:47 +0200

    array destructure

---
+++ array_destructure_copy.js
@@ -0,0 +1,9 @@
+// ? array destructure
+
+// const [firstName, lastName] = ["Subin", "Khatiwada"];
+// console.log(firstName, lastName);
+
+//const numList = [10, 20, 30];
+//console.log(numList[0]);
+const [num1, num2, num3] = [10, 20, 30];
+console.log(num2);
