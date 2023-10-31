97f1bbbe31114821413d1c636a3cc010fad204ef
Subin <sub_1n@yahoo.com>
2023-10-31 21:44:20 +0200

    more array methods

--- array.more.methods.js
+++ array.more.methods.js
@@ -18,5 +18,24 @@ const countryList = ["Nepal", "Bangladesh", "China", "India"];
 // console.log(isNepalIncluded);
 
 // ? sorts => sorts strings in alphabetical order
-countryList.sort(); // shorts in ascending order
-countryList.sort().reverse(); // sorts in descending order
+// countryList.sort(); // shorts in ascending order
+// countryList.sort().reverse(); // sorts in descending order
+//numList.sort((a, b) => a - b) returns in ascending order
+
+// ? toSorted() => Returns in a different array (does not change original array) but only runs in node version 20 or above
+// Similarly toReversed(), toSpliced() does not change original array and returns in a different array.
+
+// ? forEach => executes a provided function once for each array element. It does not return anything unlike map or filter which returns an array
+//let newList = numList.forEach((item) => console.log(item + 10));
+// let sum = 0;
+// let newList = numList.forEach((item) => {
+//   sum += item;
+// });
+// console.log(sum);
+
+// ? Reduce => reduces the array to give a single value. Solves the problem of declaring a variable like in the above foreach method. It takes a function and initial value
+
+// let reduced = numList.reduce((sum, item) => {
+//   return sum + item;
+// }, 0); // sum works like an accumulator and keep on adding each array item on it until the array ends.
+// console.log(reduced);
