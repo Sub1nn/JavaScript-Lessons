05fe245588efa667ac7dd1daa43a492f080cf856
Subin <sub_1n@yahoo.com>
2023-11-01 11:16:43 +0200

    practice session 01.11

--- homework1.js
+++ homework1.js
@@ -4,6 +4,10 @@
 // ? is all laptop priced below 2lakh
 // ?find index of laptop named "Vivobook"
 // ?return msi laptops only
+// ? sort product from cheap to expensive
+// ? sort product according to brand in descending order
+// ?calculate sum of price of all laptops
+// ?insert macbook pro and macbook air object in index position 3
 
 const laptops = [
   {
@@ -66,27 +70,68 @@ const laptops = [
 
 // ?decrease price of each laptop by 10K
 
-let newList = laptops.map((item) => ({
-  ...item,
-  price: item.price - 10000,
-}));
-//console.log(newList);
+// let newList = laptops.map((item) => ({
+//   ...item,
+//   price: item.price - 10000,
+// }));
+// console.log(newList);
 
 // ?find laptop named "TUF F15"
 
-const found = laptops.find((item) => item.name === "TUF F15");
-//console.log(found);
+// const found = laptops.find((item) => item.name === "TUF F15");
+// console.log(found);
 
 // ? is all laptop priced below 2lakh
 
-const isBelow2Lakh = laptops.every((item) => item.price < 200000);
-//console.log(isBelow2Lakh);
+// const isBelow2Lakh = laptops.every((item) => item.price < 200000);
+// console.log(isBelow2Lakh);
 
 // ?find index of laptop named "Vivobook"
 
-const vivoBook = laptops.findIndex((item) => item.name === "Vivobook");
-//console.log(vivoBook);
+// const vivoBook = laptops.findIndex((item) => item.name === "Vivobook");
+// console.log(vivoBook);
 
 // ?return msi laptops only
-const msiLaptopList = laptops.filter((item) => item.brand === "msi");
-//console.log(msiLaptopList);
+// const msiLaptopList = laptops.filter((item) => item.brand === "msi");
+// console.log(msiLaptopList);
+
+// ? sort product from cheap to expensive
+const sortedByPrice = laptops.sort((a, b) => a.price - b.price);
+//console.log(sortedByPrice);
+
+// ? sort product according to brand in descending order
+const sortedByBrandDesc = laptops.sort((a, b) =>
+  b.brand.localeCompare(a.brand)
+);
+//console.log(sortedByBrandDesc);
+
+// ?calculate sum of price of all laptops
+const sumPrice = laptops.reduce((sum, item) => {
+  return sum + item.price;
+}, 0);
+// console.log(sumPrice);
+
+// ?insert macbook pro and macbook air object in index position 3
+laptops.splice(
+  3,
+  0,
+  {
+    name: "Macbook",
+    price: 300000,
+    brand: "apple",
+  },
+  {
+    name: "Macbook Air",
+    price: 250000,
+    brand: "apple",
+  }
+);
+//console.log(laptops);
+
+// ? WAF to return key only from string "Bearer 12dfafakfafdk"
+
+getKey = (string) => {
+  return string.split(" ")[1];
+};
+const result = getKey("Bearer 12dfafakfafdk");
+console.log(result);
