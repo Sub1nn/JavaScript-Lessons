ab3b38c70e318a06272b1bc3c7ff85fb75c4a21e
Subin <sub_1n@yahoo.com>
2023-11-05 11:06:04 +0200

    async await

--- asynchronous.js
+++ asynchronous.js
@@ -1,13 +1,67 @@
-// asynchronous
+// ? asynchronous => not existing or occurring at the same time
 // pending, success, error
 
-// callback
+// ?? callback
 
-const getSum = (a, b, sum) => {
-  console.log(a, b);
+// const getSum = (a, b, callbackFunc) => {
+//   let error = true;
+//   setTimeout(() => {
+//     let total = a + b;
+//     if (error) {
+//       callbackFunc("There's an error", "We are sorry");
+//     } else {
+//       callbackFunc(null, total);
+//     }
+//   }, 1000);
+// };
+
+// getSum(3, 5, (error, result) => {
+//   console.log(error, result);
+// });
+
+// ?? promise approach then..cath
+
+// const promise = new Promise((resolve, reject) => {
+//   let error = false;
+//   setTimeout(() => {
+//     let total = 5 + 3;
+//     if (error) {
+//       reject("There's an error");
+//     } else {
+//       resolve(total);
+//     }
+//   }, 1000);
+// });
+
+// promise
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// ?? async..await
+
+// const resolvePromise = async () => {
+//   try {
+//     const res = await promise;
+//     console.log(res);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// resolvePromise();
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    console.log(res);
+    const data = await res.json();
+    //console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-//getSum(3, 5, (sum) => {});
-
-// then..cath
-// async..await
+fetchData();
