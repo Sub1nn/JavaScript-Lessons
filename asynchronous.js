36db24917b494a207272241a7a4c68468aa31a1a
Subin <sub_1n@yahoo.com>
2023-11-05 08:24:39 +0200

    asynchronous

---
+++ asynchronous.js
@@ -0,0 +1,13 @@
+// asynchronous
+// pending, success, error
+
+// callback
+
+const getSum = (a, b, sum) => {
+  console.log(a, b);
+};
+
+//getSum(3, 5, (sum) => {});
+
+// then..cath
+// async..await
